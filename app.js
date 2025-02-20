cbd05470d532adcf3c6d3fd1fa1b268992aab797
Filipe Viana <filipe.vianasilva@ufrj.br>
2025-02-20 14:18:33 -0300

    Projeto Numero Secreto

---
+++ app.js
@@ -0,0 +1,60 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do Número Secreto';
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag,texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    //console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto) {
+        let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativas}.`;
+
+        exibirTextoNaTela('h1','Acertou!');
+        exibirTextoNaTela('p',mensagemTentativas);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else if (chute > numeroSecreto) {
+        exibirTextoNaTela('p','Uhh! O chute foi maior que o número secreto');
+    } else {
+        exibirTextoNaTela('p', 'Uhh! O chute foi menor que o número secreto');
+    }
+    tentativas++
+    limparCampo()
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+
+}
